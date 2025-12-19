fe5c01e2751c1ed5a3e1d67afe78fb2f05bf028c
Roger <rogerglevin@gmail.com>
2025-12-19 12:53:57 +0000

    Implemented a REPL loop.

--- src/main.c
+++ src/main.c
@@ -4,12 +4,12 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
+  while (1==1){
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
   char command[100];
   scanf("%s",command);
   printf("%s: command not found",command);
-
+  }
   return 0;
 }
