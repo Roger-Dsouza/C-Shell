4180f0ab2e9ec511a4edda9e7acd5854fbe7f839
Roger <rogerglevin@gmail.com>
2025-12-19 15:14:30 +0000

    Hello World.

--- src/main.c
+++ src/main.c
@@ -10,6 +10,23 @@ int main(int argc, char *argv[]) {
   printf("$ ");
   char command[100];
   scanf("%s",command);
+  int a=strlen(command);
+   
+
+  char four_word[5];
+  for (int i=0;i<4;i++){
+   four_word[i]=command[i];
+  }
+  
+  if (strcmp(four_word,"echo")==0){
+   for (int i=5;i<strlen(command);i++){
+    printf("%c",command[i]);
+   } 
+   printf("\n");
+
+
+  }
+
 
   if (strcmp("exit",command)==0){
    break;
